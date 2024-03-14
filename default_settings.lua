136f9c75bf4a8668b854ba21b8aeee35f84406e4
grimmier378 <124466615+grimmier378@users.noreply.github.com>
2024-03-13 21:59:29 -0600

    Feat:
    * You can now have multiple events with their own color's for each channel.
    * FilterStrings are no longer needed. we now pass the event name along with the channel when events are triggered.
    * Default Settings are stored in a separate file for more readable code, and easy access.  and included. default_settings.lua
    * This is getting stuff ready for multiple windows.

---
+++ default_settings.lua
@@ -0,0 +1,121 @@
+return {
+	['Channels'] = {
+		['Exp AA pts'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'orange',
+					['eventString'] = '#*#gained an ability point#*#',
+				},
+				[1] = {
+					['color'] = 'yellow',
+					['eventString'] = '#*#You have gained#*#experience!#*#',
+				},
+			},
+			['enabled'] = true,
+		},
+		['Group'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You tell your party, \'#*#',
+				},
+				[1] = {
+					['color'] = 'teal',
+					['eventString'] = '#*#tells the group#*#',
+				},
+			},
+			['enabled'] = true,
+		},
+		['Say'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You say, \'#*#',
+				},
+				[1] = {
+					['color'] = 'white',
+					['eventString'] = '#*#says,#*#',
+				},
+			},
+			['enabled'] = true,
+		},
+		['Tells'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You told #1#,#*#',
+				},
+				[1] = {
+					['color'] = 'magenta',
+					['eventString'] = '#*#tells you,#*#',
+				},
+			},
+			['enabled'] = true,
+		},
+		['OOC'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You say out of character,#*#',
+				},
+				[1] = {
+					['color'] = 'dkgreen',
+					['eventString'] = '#*#say#*# out of character,#*#',
+				},
+			},
+			['enabled'] = true,
+		},
+		['Shout'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You shout, \'#*#',
+				},
+				[1] = {
+					['color'] = 'red',
+					['eventString'] = '#*#shouts,#*#',
+				},
+			},
+			['enabled'] = false,
+		},
+		['Raid'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#You tell your raid,#*#',
+				},
+				[1] = {
+					['color'] = 'dkteal',
+					['eventString'] = '#*#tells the raid#*#',
+				},
+			},
+			['enabled'] = false,
+		},
+		['Guild'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You say to your guild, \'#*#',
+				},
+				[1] = {
+					['color'] = 'green',
+					['eventString'] = '#*#tells the guild#*#',
+				},
+			},
+			['enabled'] = false,
+		},
+		['Auction'] = {
+			['Events'] = {
+				[2] = {
+					['color'] = 'grey',
+					['eventString'] = '#*#You auction,#*#',
+				},
+				[1] = {
+					['color'] = 'dkgreen',
+					['eventString'] = '#*#auctions,#*#',
+				},
+			},
+			['enabled'] = true,
+		},
+	},
+}
